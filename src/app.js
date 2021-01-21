3858bb63c431af8531dd4504a806cec0c0b6cac9
zhengwenjian <904656778@qq.com>
2021-01-21 16:16:03 +0800

    feat: 初始化模板

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import s from './global.css'
+
+export default () => {
+    return (
+        <div className={s.main}>
+            123fdsffsaf
+        </div>
+    )
+}
